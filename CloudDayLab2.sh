e41f97f4bd8c37ca27eb92ec03819c05dcb7f231
svetamorag <64458588+svetamorag@users.noreply.github.com>
2023-03-20 14:15:25 +0200

    Add files via upload

---
+++ CloudDayLab2.sh
@@ -0,0 +1,6 @@
+echo "Running a script"
+export PROJECT_ID=$(gcloud config get project)
+gcloud logging write --payload-type=json lab_log '{ "message": "The lab was created with one VM and other default settings", "custom_message": "Hi there!","personal_type":"app_log"}' --severity=INFO                                 
+gcloud logging write --payload-type=json lab_log '{ "message": "All messages from this lab will be placed in the same log", "custom_message": "This is the custom log entry for the lab","personal_type":"app_log"}' --severity=NOTICE                                 
+gcloud logging write --payload-type=json lab_log '{ "message": "There is no log sink for this project", "custom_message": "Hidden number for the quest is: 589847","personal_type":"app_log"}' --severity=NOTICE                                 
+gcloud logging write --payload-type=json lab_log '{ "message": "Please check the storage configuration for the application", "custom_message": "This is the custom log entry for the lab","personal_type":"app_log"}' --severity=ERROR                                 
